bded40b96e2375231e07ed9f56537b6f137e7866
psfrago0603 <paula@fragomeni.us>
2026-03-05 19:45:34 -0500

    Load schemas and create master build script

---
+++ Projects/2_DW_Mart_Build/build_dw_marts.sql
@@ -0,0 +1,12 @@
+-- RUN : duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
+
+-- Step 1 - DW - Create star schema tables
+
+.read 01_create_tables_dw.sql
+
+-- Step 2 - DW - Load data from CSV files into tables
+
+.read 02_load_schema_dw.sql
+
+
+
